5d6bf04b55438546ad222253745f98360d5ad615
gurmukhsingh <gurmukhs300@gmail.com>
2022-11-18 15:25:05 +1100

    adding-photos

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-  before_action :set_article, only: %i[ show edit update destroy ]
+  before_action :set_article, only: %i[show edit update destroy]
 
   # GET /articles
   def index
@@ -8,6 +8,7 @@ class ArticlesController < ApplicationController
 
   # GET /articles/1
   def show
+
   end
 
   # GET /articles/new
@@ -17,6 +18,7 @@ class ArticlesController < ApplicationController
 
   # GET /articles/1/edit
   def edit
+
   end
 
   # POST /articles
@@ -46,13 +48,14 @@ class ArticlesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_article
-      @article = Article.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def article_params
-      params.require(:article).permit(:title, :body)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def article_params
+    params.require(:article).permit(:title, :body, photos: [])
+  end
 end
